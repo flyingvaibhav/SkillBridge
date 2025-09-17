ce0d00ecf284d95283eb9af2f723fa52540195e9
Vaibhav Pandey <vaibhavpandey3186@gmail.com>
2025-09-17 16:55:52 +0530

    made login and signup page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,86 @@
-import React from 'react'
+import React, { use, useState } from 'react'
+import logo from '../assets/logo.png'
+import google from '../assets/gogglelogo.jpeg'
+import { IoEyeOutline } from "react-icons/io5";
+import { IoEye } from "react-icons/io5";
+import { useNavigate } from 'react-router-dom';
+
 
 function Login() {
+const [show , setShow]= useState(false)
+const navigate = useNavigate();
   return (
-    <div>Login</div>
+    <div className='bg-[#dddbdb] w-[100vw] h-[100vh] flex items-center justify-center '>  
+    
+    <form  className='w-[90%] md:w-200 h-150 bg-white shadow-xl rounded-2xl flex'>
+{/* left div */}
+
+
+<div className='md:w-[50%] w-[100%] h-[100%] flex flex-col items-center justify-center gap-3'>
+
+<div>
+<h1 className='font-semibold text-[black] text-2xl'> Welcome Back</h1>
+<h2 className='text-[black] text-[18px]'> Please login to your account</h2>
+</div>
+
+
+
+{/* EMAIL */}
+<div className='flex flex-col gap-1 w-[80%] items-start justify-center px-3'>
+
+
+    <label htmlFor="name" className='font-semibold'>Name</label>
+    <input type="text" id="name" className='border-1 w-[100%] h-[35px] border-[#e7e6e6] text-[15px] px-[20px ] 'placeholder='EMAIL'/>
+
+</div>
+
+{/* PASSWORD */}
+<div className='flex flex-col gap-1 w-[80%] items-start justify-center px-3 relative'>
+
+
+    <label htmlFor="password" className='font-semibold'>Password</label>
+    <input id='password' type={ show ? 'text' : 'password'} className='border-1 w-[100%] h-[35px] border-[#e7e6e6] text-[15px] px-[20px ] 'placeholder='YOUR PASSWORD'/>
+{ !show ? 
+<IoEyeOutline className='absolute w-[20px] h-[20px] cursor-pointer right-[5%] bottom-[10%]' onClick={() => setShow(prev=>!prev)} />:
+<IoEye  className='absolute w-[20px] h-[20px] cursor-pointer right-[5%] bottom-[10%]' onClick={() => setShow(prev=>!prev)} /> }
+
+
+</div>
+
+
+
+{/* submit button */}
+<button className='w-[80%] h-[40px] bg-black text-white rounded-2xl cursor-pointer flex items-center justify-center rounded-[5px]'>
+   Login
+</button>
+<div className='w-[80%] flex items-center gap-2'>
+<div className='w-[25%] h-[0.5px] bg-[#c4c4c4]'></div>
+<div className='w-[50%] text-[15px]  text-[#6f6f6f] flex items-center justify-center'>Or Continue</div>
+<div className='w-[25%] h-[0.5px] bg-[#c4c4c4]'></div>
+</div>
+
+<div  className='w-[80%] h-[40px] border-1 border-[black]   rounded-[5px] flex items-center justify-center'>
+    <img src={google} className='w-[25px]' alt="" />
+    <span className='text-[18px] text-gray-500' >Continue with Google</span>
+    
+</div>
+<div className='text-[#6f6f6f] '>Create a new account?
+  <span className='underline underline-offset-1 text-[black]' onClick={() => navigate('/signup')}>SignUp</span>
+
+</div>
+
+</div>
+
+{/* right div */}
+<div className='w-[50%] h-[100%] rounded-r-2xl bg-gray-300 md:flex items-center justify-center flex-col hidden'>
+    <img src={logo} alt=" logo " className='w-30 shadow-2xl' />
+<span  className='text-2xl text-white'>SKILLBRIDGE </span>
+</div>
+
+
+    </form>
+    
+    </div>
   )
 }
 
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/logo.png'
+import google from '../assets/gogglelogo.jpeg'
+import { IoEyeOutline } from "react-icons/io5";
+import { IoEye } from "react-icons/io5";
+import { useNavigate } from 'react-router-dom';
+
 
 function SignUp() {
-  return (
+
+    const [show , setShow]= useState(false)
+    const navigate = useNavigate();
+  return (  
     <div className='bg-[#dddbdb] w-[100vw] h-[100vh] flex items-center justify-center '>  
     
     <form  className='w-[90%] md:w-200 h-150 bg-white shadow-xl rounded-2xl flex'>
@@ -35,23 +43,45 @@ function SignUp() {
 </div>
 
 {/* PASSWORD */}
-<div className='flex flex-col gap-1 w-[80%] items-start justify-center px-3'>
+<div className='flex flex-col gap-1 w-[80%] items-start justify-center px-3 relative'>
 
 
     <label htmlFor="password" className='font-semibold'>Password</label>
-    <input type="text" id="password" className='border-1 w-[100%] h-[35px] border-[#e7e6e6] text-[15px] px-[20px ] 'placeholder='YOUR PASSWORD'/>
+    <input id='password' type={ show ? 'text' : 'password'} className='border-1 w-[100%] h-[35px] border-[#e7e6e6] text-[15px] px-[20px ] 'placeholder='YOUR PASSWORD'/>
+{ !show ? 
+<IoEyeOutline className='absolute w-[20px] h-[20px] cursor-pointer right-[5%] bottom-[10%]' onClick={() => setShow(prev=>!prev)} />:
+<IoEye  className='absolute w-[20px] h-[20px] cursor-pointer right-[5%] bottom-[10%]' onClick={() => setShow(prev=>!prev)} /> }
+
 
 </div>
 
-
+{/* for the 2 section educator and students */}
 <div className='flex md:w-[50%] w-[70%] items-center justify-between'>
-<span className='px-[10px] py-[5px]  rounded-2xl cursor-pointer'> Student</span>
-<span>EDUCATOR</span>
+<span className='px-[10px] py-[5px] border-[2px] border-[#e7e6e6] rounded-2xl cursor-pointer hover:border-black'> Student</span>
+<span className='px-[10px] py-[5px] border-[2px] border-[#e7e6e6] rounded-2xl cursor-pointer hover:border-black'> EDUCATOR</span>
+</div>
+
+{/* submit button */}
+<button className='w-[80%] h-[40px] bg-black text-white rounded-2xl cursor-pointer flex items-center justify-center rounded-[5px]'>
+    SignUp
+</button>
+<div className='w-[80%] flex items-center gap-2'>
+<div className='w-[25%] h-[0.5px] bg-[#c4c4c4]'></div>
+<div className='w-[50%] text-[15px]  text-[#6f6f6f] flex items-center justify-center'>Or Continue</div>
+<div className='w-[25%] h-[0.5px] bg-[#c4c4c4]'></div>
+</div>
+
+<div  className='w-[80%] h-[40px] border-1 border-[black]   rounded-[5px] flex items-center justify-center'>
+    <img src={google} className='w-[25px]' alt="" />
+    <span className='text-[18px] text-gray-500' >Continue with Google</span>
+    
+</div>
+<div className='text-[#6f6f6f] '>already have an account?
+  <span className='underline underline-offset-1 text-[black]' onClick={() => navigate('/login')}>Login</span>
+
 </div>
 
 </div>
-
-
 
 {/* right div */}
 <div className='w-[50%] h-[100%] rounded-r-2xl bg-gray-300 md:flex items-center justify-center flex-col hidden'>
