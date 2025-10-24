caa060749d772215587416ab595d6dd3e1464c57
Vaibhav Pandey <vaibhavpandey3186@gmail.com>
2025-10-24 23:53:25 +0530

    added some content

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -84,7 +84,7 @@ const handleLogin = async () => {
 <button className='w-[80%] h-[40px] bg-black text-white rounded-2xl cursor-pointer flex items-center justify-center rounded-[5px]' onClick={handleLogin} disabled={loading}>
    {loading ? <ClipLoader size={30} color={'white'}/> : "Login"}
 </button>
-<span className='text-[13px] cursor-pointer text-[#585757]'>Forgot your Password?</span>
+<span className='text-[13px] cursor-pointer text-[#585757]' onClick={() => navigate('/forget')}>Forgot your Password?</span>
 <div className='w-[80%] flex items-center gap-2'>
     <div className='w-[25%] h-[0.5px] bg-[#c4c4c4]'></div>
 <div className='w-[50%] text-[15px]  text-[#6f6f6f] flex items-center justify-center'>Or Continue</div>
