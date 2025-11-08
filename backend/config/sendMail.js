77670dcdf31da109d2f74a8b6501ec031445c41e
Vaibhav Pandey <vaibhavpandey3186@gmail.com>
2025-11-09 00:20:16 +0530

    created sendmail file

--- backend/config/sendMail.js
+++ backend/config/sendMail.js
@@ -1,12 +1,25 @@
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
 
 // Create a test account or replace with real credentials.
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  service: " Gmail",
   port: 465,
   secure: true, // true for 465, false for other ports
   auth: {
-    user: "maddison53@ethereal.email",
-    pass: "jn7jnAPss4f63QBp6D",
+    user: process.env.USER_EMAIL,
+    pass: process.env.USER_PASSWORD,
   },
 });
+
+const   sendMail = async (to, otp) => {
+    await transporter.sendMail({
+    from: process.env.USER_EMAIL, // sender address
+    to: to,
+    subject: "Reset Password OTP", // Subject line
+    html: `<p>Your OTP is <b>${otp}</b> It expires in 5 minutes.</p>`, // HTML body
+  })
+};
+
+export default sendMail;
