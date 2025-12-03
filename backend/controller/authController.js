5c529fbf91c8245cc0f10c8f00b1097bc01a80ce
Vaibhav Pandey <vaibhavpandey3186@gmail.com>
2025-12-03 23:41:32 +0530

    Add OTP functionality for password reset and email verification

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -2,7 +2,7 @@ import User from "../model/userModel.js";
 import validator from 'validator';
 import bcrypt from 'bcryptjs';
 import genToken from "../config/token.js";
-
+import sendMail from "../config/sendMail.js";
 
 //sign up  code 
 
@@ -86,4 +86,107 @@ export const logout = async (req,res)=>{
         return res.status(500).json({message: `Logout error ${error}`})
     }
 }
+//send otp code
+  export const sendOTP = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
 
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const otp = Math.floor(1000 + Math.random() * 9000).toString();
+
+    user.resetOTP = otp,
+    user.otpExpiry = Date.now() + 5 * 60 * 1000,// 5 minutes from now
+    user.isOtpVerified = false
+
+    await user.save();
+    await sendMail(email, otp);
+    return res.status(200).json({ message: "OTP sent to email" });
+  } catch (error) {
+    console.error("Error sending OTP:", error);
+    return res.status(500).json({ message: "Error sending OTP" });
+  }
+};
+
+//verify otp code
+export const verifyOTP = async (req, res) => {
+  try {
+    const { email, otp } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (user.resetOTP !== otp) {
+      return res.status(400).json({ message: "Invalid OTP" });
+    }
+
+    if (user.otpExpiry < Date.now()) {
+      return res.status(400).json({ message: "OTP expired" });
+    }
+
+    user.resetOTP = undefined;
+    user.otpExpiry = undefined;
+    user.isOtpVerified = true;
+
+    await user.save();
+    return res.status(200).json({ message: "OTP verified successfully" });
+
+  } catch (error) {
+    console.error("Error verifying OTP:", error);
+    return res.status(500).json({ message: "Error verifying OTP" });
+  }
+};
+
+
+//reset password code
+export const resetPassword = async (req, res) => {
+  try {
+    const { email, newPassword } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user || !user.isOtpVerified) {
+      return res.status(400).json({ message: "OTP not verified" });
+    }
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    user.password = hashedPassword;
+
+    user.isOtpVerified = false;
+    await user.save();
+    return res.status(200).json({ message: "Password reset successfully" });
+  } catch (error) {
+    console.error("Error resetting password:", error);
+    return res.status(500).json({ message: "Error resetting password" });
+  }
+};
+
+export const googleAuth = async (req, res) => {
+  try {
+    const { name, email, role } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      user= await User.create({
+        name,
+        email,
+        role
+      })
+    }
+    let token = await genToken(user._id);
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "Strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000
+    });
+
+   
+    return res.status(200).json(user);
+
+  } catch (error) {
+    return res.status(500).json({ message: `Google Auth error ${error}` });
+  }
+};
